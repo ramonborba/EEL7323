e8b444923bc0bac44412d2aca550d8611fe154fa
ramonborba <ramonborba97@gmail.com>
2021-12-16 16:51:07 -0300

    (#6 #14): Implemented class CadastroPressao based on class CadastroTemp's code

--- Exam1/src/classes/CadastroPressao/CadastroPressao.h
+++ Exam1/src/classes/CadastroPressao/CadastroPressao.h
@@ -4,3 +4,27 @@
  * Date: 15/12/2021
  * File: CadastroPressao.h
  */
+#ifndef _CADASTROPRESSAO_H_
+#define _CADASTROPRESSAO_H_
+
+#include "../Pressao/Pressao.h"
+
+#define N_PRESSURE_READINGS 20
+
+struct PressureData
+{
+    Pressao pressure;
+    bool valid = false;
+};
+
+class CadastroPressao {
+        PressureData pressureReading[N_PRESSURE_READINGS];
+        int requestID();
+    public:
+        void newReading();
+        void delReading();
+        void getReading(int reqID = -1);
+        void listReadings();
+};
+
+#endif /* _CADASTROPRESSAO_H_ */
