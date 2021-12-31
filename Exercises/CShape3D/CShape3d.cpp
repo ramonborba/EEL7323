5665a40e9f3b31ca3f95008dd5b8c82d3df327df
ramonborba <ramonborba97@gmail.com>
2021-12-31 12:36:02 -0300

    (#17 #19): Implemented initial CShape3dclass declaration

---
+++ Exercises/CShape3D/CShape3d.cpp
@@ -0,0 +1,15 @@
+/**
+ * File: CShape3d.cpp
+ * 
+ * Author: Ramon de Araujo Borba < ramonborba97@gmail.com >
+ * Institution: UFSC
+ * Date: 15/12/2021
+ * 
+ * Description: CShape3d abstract class for CShape3d assingment
+ * 
+ */
+
+class CShape3d {
+    public:
+        virtual float volume() = 0;
+};
