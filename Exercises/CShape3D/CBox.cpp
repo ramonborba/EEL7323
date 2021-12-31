bd2bec7d93c44fb31aab507c980792efc4e4d0b5
ramonborba <ramonborba97@gmail.com>
2021-12-31 12:46:51 -0300

    (#17 #20): Implemented initial Cbox class declaration

---
+++ Exercises/CShape3D/CBox.cpp
@@ -0,0 +1,26 @@
+/**
+ * File: CBox.cpp
+ * 
+ * Author: Ramon de Araujo Borba < ramonborba97@gmail.com >
+ * Institution: UFSC
+ * Date: 15/12/2021
+ * 
+ * Description: CBox class for CShape3d assingment
+ * 
+ */
+
+class CBox : public CShape3d {
+    private:
+        float h, w, l;                  // Attributes for height (h), width (w) and length (l)
+    public:
+        CBox(float, float, float);      // Constructor for CBox, receiving h, w and l as parameters
+        float volume();
+};
+
+CBox::CBox(float h, float w, float l){
+
+}
+
+float CBox::volume(){
+    
+}
