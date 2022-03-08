0d8a339477f621d7ef25dd672cd800a76534e786
Ramon Borba <ramonborba97@gmail.com>
2022-03-08 17:30:10 -0300

    (#24 #43 #27): Initial Node class declaration

---
+++ FinalProject/Code/pc/src/Queue/Node.cpp
@@ -0,0 +1,36 @@
+/**
+ * Author: Ramon de Araujo Borba < ramonborba97@gmail.com >
+ * Institution: UFSC
+ * Date: 08/03/2022
+ * File: Node.cpp
+ */
+
+#include "Node.h"
+
+Node::Node(int data, Node* nxt)
+{
+}
+
+Node::~Node()
+{
+}
+
+int Node::getData()
+{
+    
+}
+
+Node* Node::getNext()
+{
+
+}
+
+void Node::setData(int data)
+{
+
+}
+
+void Node::setNext(Node* nxt)
+{
+    
+}
---
+++ FinalProject/Code/pc/src/Queue/Node.h
@@ -0,0 +1,22 @@
+/**
+ * Author: Ramon de Araujo Borba < ramonborba97@gmail.com >
+ * Institution: UFSC
+ * Date: 08/03/2022
+ * File: Node.h
+ */
+
+
+
+class Node
+{
+protected:
+    int datapkt;
+    Node* next;
+public:
+    Node(int datapkt, Node* next);
+    ~Node();
+    int getData();
+    Node* getNext();
+    void setData(int data);
+    void setNext(Node* nxt);
+};
