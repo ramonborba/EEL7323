3dbdfd16202a291b466b260bf06deb9ad8e0cfc3
Ramon Borba <ramonborba97@gmail.com>
2022-03-23 17:13:51 -0300

    (#44): Added missing include guards to headers

--- FinalProject/Code/embsys/SmartAC/tasks/gpio_event_task/include/gpio_event_task.hpp
+++ FinalProject/Code/embsys/SmartAC/tasks/gpio_event_task/include/gpio_event_task.hpp
@@ -5,5 +5,14 @@
  * File: gpio_event_task.hpp
  */
 
+#ifndef GPIO_EVENT_TASK_HPP_
+#define GPIO_EVENT_TASK_HPP_
 
+/**
+ * @brief Task to handle the button interrupts
+ * 
+ * @param pvParameters Necessary for task creation, but no used in this task, set to NULL on task creation
+ */
 void gpio_event_task(void *pvParameters);
+
+#endif /* GPIO_EVENT_TASK_HPP_ */
